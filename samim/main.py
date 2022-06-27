558368eaaa286202ca287641bc169b428d0792c9
king-samim <106947205+king-samim@users.noreply.github.com>
2022-06-27 09:48:53 +0430

    Create main.py
    my file

---
+++ samim/main.py
@@ -0,0 +1,34 @@
+import pygame
+import os
+
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Spaceship Female")
+WHITE = (255,255,255)
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, 
+                                                        (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, 
+                                                        (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (300, 100))
+    WIN.blit(RED_SPACESHIP, (700, 100))
+    pygame.display.update()
+
+def main():
+    run = True
+    while(run):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
